760105b2a2170af9f81dd3b8fdbc81c4d4235c59
George Featherstone <DevCabin@users.noreply.github.com>
2014-05-27 17:48:08 -0500

    Update content-single.php
    Adding featured image support

--- templates/content-single.php
+++ templates/content-single.php
@@ -1,6 +1,13 @@
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
     <header>
+
+  <?php if ( has_post_thumbnail() ) { ?>
+  	<div class="page_img">
+  	 	<?php the_post_thumbnail(); ?>
+  	</div>
+  <?php } ?>
+    
       <h1 class="entry-title"><?php the_title(); ?></h1>
       <?php get_template_part('templates/entry-meta'); ?>
     </header>
