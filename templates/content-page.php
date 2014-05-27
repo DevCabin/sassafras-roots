7bc5660cff8d589ddcc0013c4be2c94bc4652135
George Featherstone <DevCabin@users.noreply.github.com>
2014-05-27 17:47:22 -0500

    Update content-page.php
    Adding support for featured image

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,4 +1,11 @@
 <?php while (have_posts()) : the_post(); ?>
+
+<?php if ( has_post_thumbnail() ) { ?>
+	<div class="page_img">
+	 	<?php the_post_thumbnail(); ?>
+	</div>
+<?php } ?>
+
   <?php the_content(); ?>
   <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
 <?php endwhile; ?>
