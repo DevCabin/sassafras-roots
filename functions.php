4a028c220109677214fd2703d97f77203000c2cb
George Featherstone <DevCabin@users.noreply.github.com>
2014-07-15 08:21:31 -0500

    Update functions.php
    Added 'limit words' function to excerpt

--- functions.php
+++ functions.php
@@ -312,5 +312,11 @@ function theme_front_page_settings() {
 add_action("admin_init", "my_theme_admin_init");
 add_action("admin_menu", "setup_theme_admin_menus");
 
-
+function string_limit_words($string, $word_limit)
+{
+  $words = explode(' ', $string, ($word_limit + 1));
+  if(count($words) > $word_limit)
+  array_pop($words);
+  return implode(' ', $words);
+}
 
