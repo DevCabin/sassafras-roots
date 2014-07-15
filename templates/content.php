85f204741c624b35540d7e9f36641f0276724440
George Featherstone <DevCabin@users.noreply.github.com>
2014-07-15 09:14:38 -0500

    Update content.php
    Added if / else for post_thumbnail support regardless of category / taxonomy

--- templates/content.php
+++ templates/content.php
@@ -1,4 +1,26 @@
+<?php if ( has_post_thumbnail() ) { ?>
+
+ <article <?php post_class(); ?>>
+ <div class="col-md-3">
+    <div class="post_img">
+  	 	<?php the_post_thumbnail(); ?>
+  	</div>
+ </div>
+ <div class="col-md-9">
+	  <header>
+		<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+		<?php get_template_part('templates/entry-meta'); ?>
+	  </header>
+	  <div class="entry-summary">
+		<?php the_excerpt(); ?>
+	  </div>
+  </div>
+</article> 	
+
+<?php } else { ?>
+
 <article <?php post_class(); ?>>
+
   <header>
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <?php get_template_part('templates/entry-meta'); ?>
@@ -7,3 +29,5 @@
     <?php the_excerpt(); ?>
   </div>
 </article>
+
+<?php } ?>
